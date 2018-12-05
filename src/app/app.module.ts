671e0a0dd25690a167c18d7c0c0133bd5b61d4b4
Xurmein <wj.pavocrist@gmail.com>
2018-12-05 13:43:02 -0500

    JPdev commit 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent,
+   
   ],
   imports: [
     BrowserModule,
